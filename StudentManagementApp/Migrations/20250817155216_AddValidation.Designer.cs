38123c3d246514792e8aeca9f5f2e10d6b0d6e40
Sumon K. Mondol <Sumon@techsflow.com>
2025-08-17 21:58:23 +0600

    add readme and validation

--- StudentManagementApp/Migrations/20250817155216_AddValidation.Designer.cs
+++ StudentManagementApp/Migrations/20250817155216_AddValidation.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using StudentManagementSystem.Data;
 
@@ -10,9 +11,11 @@ using StudentManagementSystem.Data;
 namespace StudentManagementApp.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250817155216_AddValidation")]
+    partial class AddValidation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- StudentManagementApp/Models/Student.cs
+++ StudentManagementApp/Models/Student.cs
@@ -5,11 +5,12 @@ namespace StudentManagementApp.Models
     public class Student
     {
         public int Id { get; set; }
-
+        [Display(Name="First Name")]
         [Required(ErrorMessage = "First name is required")]
         [StringLength(50, ErrorMessage = "First name cannot be longer than 50 characters")]
         public string FirstName { get; set; }
 
+        [Display(Name = "Last Name")]
         [Required(ErrorMessage = "Last name is required")]
         [StringLength(50, ErrorMessage = "Last name cannot be longer than 50 characters")]
         public string LastName { get; set; }
